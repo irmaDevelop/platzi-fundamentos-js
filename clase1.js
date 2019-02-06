53d44dcdd56c050974dae9abb22f04f2cd801f85
IrmaJulia <irmajuliadavila@icloud.com>
2019-02-05 19:28:02 -0500

    variables commit

---
+++ clase1.js
@@ -0,0 +1,10 @@
+//console.log('Hola mundo JS!')
+//js NO obliga a poner ; al final excepto en algunos casitos
+var nombre = 'Irma'
+var apellido = 'Dávila'
+var edad = 35
+edad = '35 años' //la definicion de variable puede cambiar 
+//por eso javascript es un lenguaje DEBILMENTE TIPADO
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad)
+peso = 64
